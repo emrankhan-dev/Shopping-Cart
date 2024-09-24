68b4dd861df2a0c2bb4ced534b602c8908a8306c
Emran khan <emrankhan@Emrans-MacBook-Air.local>
2024-09-25 00:20:23 +0600

    Initially apps are running

--- index.js
+++ index.js
@@ -13,32 +13,49 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
-// UI-EL
+
 const inputEl = document.getElementById("input-el")
 const addBtnEl = document.getElementById("add-btn-el")
-const clearAllBtnEl = document.getElementById("clear-all-btn-el")
 const shoppingListEl = document.getElementById("shoppingList-el")
 
 addBtnEl.addEventListener("click", function () {
     push(shoppingListInDB, inputEl.value)
-    inputEl.value = ""
+    clearInputField()
 })
 
 onValue(shoppingListInDB, function (snapshot) {
-    let snapshotExists = snapshot.exists()
-    if (snapshotExists) {
-            let itemsArray = Object.values(snapshot.val())
-            shoppingListEl.innerHTML = ""
-            for (let i = 0; i < itemsArray.length; i++) {
-            appendItemsToShoppingListEL(itemsArray[i])
+    if(snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+        clearItemsFromShoppingList()
+        for(let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemId = currentItem[0]
+            let currentItemValue = currentItem[1]
+            appendItemsToShoppingListEL(currentItem)
         }
+    } else {
+        shoppingListEl.innerHTML = "No items here ....yet"
     }
-    
-    
 })
 
 function appendItemsToShoppingListEL(item) {
-    shoppingListEl.innerHTML += `<li>${item}</li>`
+    let itemId = item[0]
+    let itemValue = item[1]
+    let newEl = document.createElement('li')
+    newEl.textContent = itemValue
+    newEl.addEventListener('dblclick', function() {
+        let exactItemLocationInDB = ref(database, `shoppingList/${itemId}`)
+        remove(exactItemLocationInDB)
+    })
+    shoppingListEl.append(newEl)
+}
+
+function clearInputField() {
+    inputEl.value = ""
+}
+
+function clearItemsFromShoppingList() {
+    shoppingListEl.innerHTML = ""
 }
 
 
